0349ce9da1e5fefdb129742f7481b445b84f82c2
Fernando Perez <feragusper@gmail.com>
2022-12-09 13:25:01 +0100

    Background color in home adjusted.

--- features/home/presentation/src/main/java/com/feragusper/home/presentation/mvi/HomeViewState.kt
+++ features/home/presentation/src/main/java/com/feragusper/home/presentation/mvi/HomeViewState.kt
@@ -1,6 +1,7 @@
 package com.feragusper.home.presentation.mvi
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
@@ -33,7 +34,8 @@ class HomeViewState : MVIViewState<HomeIntent> {
             Column(
                 modifier = Modifier
                     .padding(it)
-                    .fillMaxSize(),
+                    .fillMaxSize()
+                    .background(MaterialTheme.colors.surface),
                 verticalArrangement = Arrangement.SpaceEvenly,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
